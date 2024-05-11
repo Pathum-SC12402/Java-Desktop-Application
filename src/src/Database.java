9906a80c808690ab3385102417adf9ac7301b277
Pathum Dilshan <156510213+Pathum-SC12402@users.noreply.github.com>
2024-05-11 14:06:52 +0530

    change database.java file

--- src/src/Database.java
+++ src/src/Database.java
@@ -9,52 +9,27 @@ import java.util.logging.Logger;
  * @author User
  */
 public class Database {
-     private static Database instance;
-    
-     public static Database getInstance() {
-        // Create the instance if it's null
-        if (instance == null) {
-            instance = new Database();
-            
-            String url = "jdbc:sql6.freesqldatabase.com:3306/sql6702245";
-            String user = "sql6702245";
-            String pass = "5EjAx8cGNr";
-            
-            try {
-                instance.connect(url, user, pass);
-                System.out.println("connected to database");
-            } catch (java.lang.Exception ex) {
-                Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-                instance.con = null;
-            }
+    private static Database instance;
+    public Connection con;
+    private Database() {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=(Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/wildlifedatabase","root","bbw@8138");
+            System.out.println("Connection Successfully...");
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
         }
+    }
+    
+    public static Database getInstance(){
+        if(instance == null)
+            instance = new Database();
         return instance;
     }
     
-    private Database(){}
-    
-    public Connection con;
-    
-    public void connect(String url, String username, String pass) throws java.lang.Exception{
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(url, username, pass);
-            System.out.println("Connected");
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-            throw Exception("Connection error");
-        }
-    }
-    
-    
-//    public void runSql(String q){
-//        
-//    }
-
-    private Exception Exception(String connection_error) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
-    
-     
+    public Connection getConnection(){
+        return con;
+    }       
 }
